debf2eb8325a948472613c51be895203f2871e79
Josh Woomin Park <joshuaparkwm@gmail.com>
2020-11-13 21:22:14 +0900

    Initial Commit

---
+++ LazyMock/LazyMock.h
@@ -0,0 +1,18 @@
+//
+//  LazyMock.h
+//  LazyMock
+//
+//  Created by Josh Woomin Park on 2020/11/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LazyMock.
+FOUNDATION_EXPORT double LazyMockVersionNumber;
+
+//! Project version string for LazyMock.
+FOUNDATION_EXPORT const unsigned char LazyMockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LazyMock/PublicHeader.h>
+
+
