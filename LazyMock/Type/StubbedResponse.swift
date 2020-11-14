ac93c96f78f553079278310e895bdf934119d8e7
Josh Woomin Park <joshuaparkwm@gmail.com>
2020-11-14 09:53:18 +0900

    Add: StubbedResponse

---
+++ LazyMock/Type/StubbedResponse.swift
@@ -0,0 +1,13 @@
+//
+//  StubbedResponse.swift
+//  LazyMock
+//
+//  Created by Josh Woomin Park on 2020/11/14.
+//
+
+import Foundation
+
+enum StubbedResponse {
+    case value(Any)
+    case error(Error)
+}
