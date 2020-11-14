14475329e919cbd07e56ac09801f1d4e45529722
Josh Woomin Park <joshuaparkwm@gmail.com>
2020-11-14 10:08:49 +0900

    Update: StubProviding - canStub flag

--- LazyMock/Protocol/StubProviding.swift
+++ LazyMock/Protocol/StubProviding.swift
@@ -10,6 +10,14 @@ import Foundation
 protocol StubProviding {
     
     /**
+     Tells if the precondition is met before calling `stub()`.
+     
+     If this value is `false`, do **NOT** call `stub()`.
+     */
+    var canStub: Bool { get }
+    
+    /**
+     - Precondition: `canStub == true`.
      - Returns: The stubbed response.
      */
     mutating func stub() -> StubbedResponse
