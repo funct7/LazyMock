5f765c2b7a027523ea17cba5dbf2393f49aa2a91
Josh Woomin Park <joshuaparkwm@gmail.com>
2020-11-14 09:24:30 +0900

    Add: Stubbing protocol

---
+++ LazyMock/Stubbing.swift
@@ -0,0 +1,68 @@
+//
+//  Stubbing.swift
+//  LazyMock
+//
+//  Created by Josh Woomin Park on 2020/11/14.
+//
+
+import Foundation
+
+public protocol Stubbing {
+    
+    /**
+     Stubs the result of a method call `count` number of times. (Used by the test client code.)
+  
+     The behavior is accumulative, so if the client calls this method with `count` value as `2`,
+     and then calls this method with a different `returnValue` with a `count` of `1`,
+     the first two calls will return the first `returnValue` and the third call will return the next `returnValue` once.
+     
+     Subsequent calls will crash due to `nil` force-unwrapping unless a default stub value is set with `count` as `0`.
+     
+     The accumulative effect is shared with `when(_:isCalledThrow:numberOfTimes:)`.
+     To reset the recorded stub behavior, call `resetStubBehavior()`.
+     
+     - Parameters:
+         - methodName: The method whose result  to stub.
+         - returnValue: The value to return.
+         - count: The number of times to stub. Use `0` to set the default stub value.
+     */
+    func when(
+        _ methodName: String,
+        isCalledReturn returnValue: Any,
+        numberOfTimes count: UInt)
+    
+    /**
+     Stubs the throwing behavior of a method call `count` number of times. (Used by the test client code.)
+     
+     The behavior is accumulative, so if the client calls this method with `count` value as `2`,
+     and then calls this method with a different `error` with a `count` of `1`,
+     the first two calls will throw the first `error` and the third call will throw the next `error` once.
+     
+     Subsequent calls will crash due to `nil` force-unwrapping unless a default stub value is set with `count` as `0`.
+     
+     The accumulative effect is shared with `when(_:isCalledReturn:numberOfTimes:)`.
+     To reset the recorded stub behavior, call `resetStubBehavior()`.
+     
+     - Parameters:
+         - methodName: The method whose result  to stub.
+         - returnValue: The value to return.
+         - count: The number of times to stub. Use `0` to set the default stub value.
+     */
+    func when(
+        _ methodName: String,
+        isCalledThrow error: Error,
+        numberOfTimes count: UInt)
+    
+    /**
+     Resets the recorded stub behavior. (Used by the test client code.)
+     */
+    func resetStubBehavior()
+    
+    /**
+     Provides a stubbed response to the method call. (Used by the manually mocked object.)
+     
+     If `when(_:isCalledThrow:numberOfTimes:)` was not invoked, no error will be thrown; i.e. it is safe to call with `try!`.
+     */
+    func stub<T>(_ methodName: String) throws -> T?
+    
+}
